c30fc164480f619c6e5fe77cfc948faa2de5739f
Adam Firdaus Tan <adambft98@gmail.com>
2022-10-06 23:31:09 +0800

    testt

--- js1.js
+++ js1.js
@@ -19,3 +19,38 @@ function create_draw_table() {
 
     div_elem_to_add_table.innerHTML = new_table
 }
+
+function night_mode() {
+    //check if alr night mode
+    let night_btn_elem = document.getElementById("night-btn")
+
+    let bright_arr = ["dark-bg", "light-bg-1", "light-bg-2", "light-bg-3", "light-bg-4"]
+    let night_arr = ["dark-bg-night", "light-bg-1-night", "light-bg-2-night", "light-bg-3-night", "light-bg-4-night"]
+
+    let to_find = bright_arr
+    let change_to = night_arr
+
+    if (night_btn_elem.hasAttribute("night_mode")) {
+        to_find = night_arr
+        change_to = bright_arr
+        night_btn_elem.removeAttribute("night_mode")
+    } else {
+        night_btn_elem.setAttribute("night_mode", "")
+    }
+
+    //do thing
+    for (e_idx in to_find) {
+        e_to_find = to_find[e_idx]
+        
+        let curr_arr_to_edit = document.getElementsByClassName(e_to_find)
+
+        // console.log(e_to_find)
+        // console.log(curr_arr_to_edit)
+        // console.log("----------------------------------------------------")
+
+        for (e_elem_to_change of curr_arr_to_edit) {
+            e_elem_to_change.classList.toggle(e_to_find)
+            e_elem_to_change.classList.toggle(change_to[e_idx])
+        }
+    }
+}
